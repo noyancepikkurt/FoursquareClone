19cdcae958ee5a67bd17ce687ab4d12dba08aaf5
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-09-29 19:24:04 +0300

    Kullanıcıdan aldığımız bilgileri Parse'a yüklemeyi hallettim. TableView'a Parse'dan gelecek olan bilgileri çekeceğim.

--- FourSquareClone/MapVC.swift
+++ FourSquareClone/MapVC.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import MapKit
+import Parse
 
 class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
+    var chosenLatitude = ""
+    var chosenLongitude = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +30,39 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         
-        
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation))
+        recognizer.minimumPressDuration = 2.5
+        mapView.addGestureRecognizer(recognizer)
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    @objc func chooseLocation(gestureRecognizer : UIGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizer.State.began {
+            
+            let touches = gestureRecognizer.location(in: self.mapView)
+            let coordinates = self.mapView.convert(touches, toCoordinateFrom: self.mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates
+            annotation.title = PlaceModel.sharedInstance.placeName
+            annotation.subtitle = PlaceModel.sharedInstance.placeType
+            
+            self.mapView.addAnnotation(annotation)
+            
+            PlaceModel.sharedInstance.placeLatitude = String(coordinates.latitude)
+            PlaceModel.sharedInstance.placeLongitude = String(coordinates.longitude)
+        }
+    }
+    
+    
     
     @objc func backButtonClicked() {
         
@@ -40,9 +74,29 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @objc func saveButtonClicked() {
         
-        // PARSE
+        let placeModel = PlaceModel.sharedInstance
         
+        let object = PFObject(className: "Places")
+        object["Name"] = placeModel.placeName
+        object["Type"] = placeModel.placeType
+        object["Atmosphere"] = placeModel.placeAtmosphere
+        object["Latitude"] = placeModel.placeLatitude
+        object["Longitude"] = placeModel.placeLongitude
+        
+        if let imageData = placeModel.placeImage.jpegData(compressionQuality: 0.5) {
+            object["Image"] = PFFileObject(name: "image.jpeg", data: imageData)
+        }
+        
+        object.saveInBackground { success, error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "ok", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+               
+            } else {
+                self.performSegue(withIdentifier: "fromMapVCtoPlacesVC", sender: nil)
+            }
+        }
     }
-    
-
 }
